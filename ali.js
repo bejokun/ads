b05e4ec8baa186b4199189e188f0854cf0f1499d
bejokun <bejokun34@gmail.com>
2021-09-23 18:15:28 +0700

    update key

--- ali.js
+++ ali.js
@@ -5,7 +5,7 @@ function gasken() {
 	if (permalink.includes("/item/")) {
 		var rawid = permalink.match("item\/(.*)\.html")[0];
 		var idne = rawid.replace("item/", "");
-		var aff = "http://s.click.aliexpress.com/deep_link.htm?dl_target_url=https://www.aliexpress.com/item/-/ikigantinenbosqu&aff_short_key=_AKzWuz";
+		var aff = "http://s.click.aliexpress.com/deep_link.htm?dl_target_url=https://www.aliexpress.com/item/-/ikigantinenbosqu&aff_short_key=_9iRZpY";
 		var lempar = aff.replace("ikigantinenbosqu", idne);
 
 		if (['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
