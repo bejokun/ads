0bc4f1744226fd2fd0f6c0d0e65db30042c99a26
bejokun <bejokun34@gmail.com>
2021-09-25 15:25:11 +0700

    tes2

--- ali.js
+++ ali.js
@@ -1,17 +1,17 @@
-/** MIKIR DEWE COK by SH **/
+window.onload = gasken() {
+	var permalink = window.location.pathname;
+	if (permalink.includes('/item/')) {
+		var rawid = permalink.match('item\/(.*)\.html')[0];
+		var idne = rawid.replace('item/', '');
+		var aff = 'https://gobuy.cyou/go.php?http://s.click.aliexpress.com/deep_link.htm?dl_target_url=https://www.aliexpress.com/item/-/ikigantinenbosqu&aff_short_key=_9iRZpY';
+		var lempar = aff.replace('ikigantinenbosqu', idne);
 
-var permalink = window.location.pathname;
-if (permalink.includes('/item/')) {
-	var rawid = permalink.match('item\/(.*)\.html')[0];
-	var idne = rawid.replace('item/', '');
-	var aff = 'https://gobuy.cyou/go.php?http://s.click.aliexpress.com/deep_link.htm?dl_target_url=https://www.aliexpress.com/item/-/ikigantinenbosqu&aff_short_key=_9iRZpY';
-	var lempar = aff.replace('ikigantinenbosqu', idne);
-
-	if (['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
-		window.location.href = lempar
-	}
-} else {
-	if (['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
-		window.location.href = 'https://gobuy.cyou/go.php?https://s.click.aliexpress.com/e/_A0OHom'
+		if (['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
+			window.location.href = lempar
+		}
+	} else {
+		if (['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
+			window.location.href = 'https://gobuy.cyou/go.php?https://s.click.aliexpress.com/e/_A0OHom'
+		}
 	}
 }
